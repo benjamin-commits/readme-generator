fd10be639244af82557ad6fdfa5ffd87aca51ab8
benyunus1 <beyazidy@gmail.com>
2020-09-27 13:10:40 -0500

    many changes

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,84 @@
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const writeFileAsync = util.promisify(fs.writeFile);
+
 // array of questions for user
-const questions = [
-
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
+function promptUser(){
+    return inquirer.prompt([
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project: ",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Write a brief description of your project: "
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Describe the installation process, if any: ",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What will the project be used for: "
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose the appropriate license for this project: ",
+        choices: [
+            "Apache",
+            "Academic",
+            "GNU",
+            "ISC",
+            "MIT",
+            "Mozilla",
+            "Open"
+        ]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Name the contributors of this projects: "
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Is there a test included? "
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "What do I do if I have an issue? "
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "Please enter your GitHub username: "
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email: "
+    }
+])};
+// function to generate README
+async function init() {
+    try {
+        // Ask user questions and generate responses
+        const data = await promptUser();
+        const generateContent = generateMarkdown(data);
+        // Write new README.md to dist directory
+        await writeFileAsync('./Develop/mdfile/README.md', generateContent);
+    }   catch(err) {
+        console.log(err);
+    }
+  }
+  
+  init();  
+ 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,44 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# <h1 align="center">${data.title}</h1>
+  
+  ![badge](https://img.shields.io/badge/license-${data.license}-blue)
 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ![badge](https://img.shields.io/badge/license-${data.license}-blue)
+
+  This application allows licensing by the ${data.license} license.
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  ${data.questions}
+  Check me out on Github: [${data.username}](https://github.com/${data.username})
+  Feel free to email me with any questions: ${data.email}
+
+  _This README was carefully crafted by [readme-generator](https://github.com/benyunus1/readme-generator)_
 `;
 }
 
